2c957e4929bc8e8aff33bfbbfdd81a5a8650f2e3
ruiz9984-nbjuKP <ruiz9984@icloud.com>
2023-01-06 18:43:45 -0500

    some css

--- controllers/index.js
+++ controllers/index.js
@@ -8,22 +8,3 @@ router.use('/api', apiRoutes);
 
 module.exports = router;
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-//handlebar route
-app.get('/',function(req, res){
-    res.render('homepage');
-});
-app.get('/login',function(req, res){
-    res.render('login');
-});
-app.get('/recipes',function(req, res){
-    res.render('recipes');
-});
-app.get('/shop',function(req, res){
-    res.render('shop');
-});
-
-app.use(express.static(path.join(_dirnmae,'public')));
-
-app.listen(PORT, () => console.log('Server listening on port' + PORT));
